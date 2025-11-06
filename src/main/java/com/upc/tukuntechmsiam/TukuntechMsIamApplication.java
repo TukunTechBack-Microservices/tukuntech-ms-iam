45171c8ecf3feae194d2241a723e7812a2ab71d4
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-06 13:36:05 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/tukuntechmsiam/TukuntechMsIamApplication.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechmsiam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukuntechMsIamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukuntechMsIamApplication.class, args);
+    }
+
+}
